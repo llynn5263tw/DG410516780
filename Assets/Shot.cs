a13038487193e2e5ee7bf6a4f610c4327c483871
llynn5263tw <llynn5263tw@gmail.com>
2018-04-20 11:58:59 +0800

    finally

---
+++ Assets/Shot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+
+    public float speed;
+    new Rigidbody rigidbody;
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.velocity = transform.forward * speed;
+        Destroy(gameObject, 10);
+    }
+}
---
+++ Assets/TankBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankBullet : MonoBehaviour
+{
+    public float speed;
+    public GameObject shot;
+    public float fireRate;
+    public Transform shotSpawn;
+    float nextFire;
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, -30 * Time.deltaTime * speed, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 30 * Time.deltaTime * speed, 0);
+        }
+    }
+}
